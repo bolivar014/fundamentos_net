e69fc7c53daf27b7292a4cf4dc51e63949274db3
Juan Camilo Valdes Bolivar <bolivar014@gmail.com>
2022-08-21 16:29:29 -0500

    compilación y ejecución

---
+++ Program.cs
@@ -0,0 +1,14 @@
+﻿// See https://aka.ms/new-console-template for more information
+// Console.WriteLine("Hello, World!");
+Console.WriteLine("Por favor ingrese su nombre");
+// Leemos
+var nombre = Console.ReadLine();
+
+Console.WriteLine("Por favor ingrese su cargo");
+var cargo = Console.ReadLine();
+
+Console.WriteLine("Ingrese su edad");
+var edad = Console.ReadLine();
+
+
+Console.WriteLine($"Mi nombre es: {nombre}, trabajo como: {cargo} y tengo: {edad} años");
