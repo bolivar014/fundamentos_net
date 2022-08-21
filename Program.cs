8de2467f799587b4d7aca0efd6e45a3889440689
Juan Camilo Valdes Bolivar <bolivar014@gmail.com>
2022-08-21 17:04:47 -0500

    integrando nuget - humanizer

--- Program.cs
+++ Program.cs
@@ -1,5 +1,6 @@
-﻿// See https://aka.ms/new-console-template for more information
-// Console.WriteLine("Hello, World!");
+﻿// Importamos nugget humanizer
+using Humanizer;
+
 Console.WriteLine("Por favor ingrese su nombre");
 // Leemos
 var nombre = Console.ReadLine();
@@ -8,7 +9,6 @@ Console.WriteLine("Por favor ingrese su cargo");
 var cargo = Console.ReadLine();
 
 Console.WriteLine("Ingrese su edad");
-var edad = Console.ReadLine();
+var edad = int.Parse(Console.ReadLine());
 
-
-Console.WriteLine($"Mi nombre es: {nombre}, trabajo como: {cargo} y tengo: {edad} años");
+Console.WriteLine($"Mi nombre es: {nombre}, trabajo como: {cargo} y tengo: {edad.ToWords(new System.Globalization.CultureInfo("es"))} años");
